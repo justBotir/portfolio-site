f438e9adb1b435c55fe756e6160b1acc70063da8
Qakhramoniy-hub <raimboyevbotir@gmail.com>
2026-03-01 00:08:20 +0500

    Initial clean commit

---
+++ components/Social.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+import { FaGithub, FaLinkedinIn, FaTwitter, FaTelegram } from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "https://github.com/Qakhramoniy-hub" },
+  { icon: <FaLinkedinIn />, path: "https://linkedin.com/in/BotirQakhramoniy" },
+  { icon: <FaTelegram />, path: "https://t.me/Botir_Qakhramoniy" },
+  { icon: <FaTwitter />, path: "" },
+];
+
+const Social = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Social;
